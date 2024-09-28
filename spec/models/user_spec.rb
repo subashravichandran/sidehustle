e1df92b4cc8151cef0a71ed5bc7665b7ef662370
subash <waytosubash@gmail.com>
2024-09-28 20:15:59 +0530

    User model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  fixtures :users, :alerts
+  let!(:user) { User.create(name: 'Tony Stark', dob: '1990-05-21') }
+  let!(:alert) { Alert.create(user: user, content: 'Happy Birthday', trigger_at: '1990-05-21 09:00:00') }
 
-  it 'has a valid name' do
-    user = users(:ironman)
-    expect(user.name).to be_present
-    expect(user.dob).to be_present
-    expect(user.name).to eq('Tony Stark')
-    expect(user.alerts).to be_present
+  context 'Model: has valid' do
+    it 'user record' do
+      expect(user.name).to be_present
+      expect(user.dob).to be_present
+      expect(user.name).to eq('Tony Stark')
+    end
+
+    it 'alert record for user' do
+      expect(user.alerts).to be_present
+      expect(user.alerts.id).to eq(5)
+    end
   end
 end
